567af28a47ebc330bc38a29e3c5ae857be5e9a8d
milad <msherwani92@gmail.com>
2022-04-30 15:56:59 +0300

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 void main() {
   runApp(const MyApp());
 }
@@ -35,16 +37,69 @@ class _MyHomePageState extends State<MyHomePage> {
        
         title: Text("APi"),
       ),
-      body: Center(
-       
-        child: Column(
-         
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            
-          ],
+      body: Container(
+        child: FutureBuilder<List<dynamic>>(
+           future: getAPI(),
+           builder: (context,snapshot){
+              if(!snapshot.hasData){
+
+                   return Center(child: CircularProgressIndicator());
+
+              } else if(snapshot.hasError){
+
+                   return Text(snapshot.error.toString());
+
+              }else{
+
+                  return ListView.builder(
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (context,index){
+
+                      return Container(
+                        height: 70,
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15.0)
+                          ),
+                          color:index % 2==0 ?  Colors.deepPurple .shade200 : Colors.lightGreen.shade300,
+
+                          margin: EdgeInsets.all(8.0),
+
+                          child: Padding(
+                            padding: EdgeInsets.all(6),
+                            child: Text(snapshot.data![index],
+                            style: TextStyle(
+                              color: index % 2==0 ?  Colors.black  :Colors.white ,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                          ),
+                              ),
+                            );
+                    },
+                    
+                    );
+
+              }
+           },
+
         ),
       ),
     );
   }
+
+Future<List<dynamic>> getAPI()async{
+  String theUri ="https://jsonplaceholder.typicode.com/albums";
+   http.Response response= await http.get(Uri.parse(theUri));
+
+List decodedJson=jsonDecode(response.body);
+  
+  return  decodedJson.map((e) => e["title"]).toList();
+}
+
+
+
+
+
 }
